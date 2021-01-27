f61407ad2da13602ec8f28f0687246d8719dd4c1
EthanBayer <bayeres1871@gmail.com>
2021-01-26 21:19:44 -0800

    Added basic Board class framework.

---
+++ headers/board.hpp
@@ -0,0 +1,13 @@
+#ifndef BOARD_HPP
+#define BOARD_HPP
+
+#include <iostream>
+
+class Board
+{
+    protected:
+        virtual void initialize() = 0;
+
+};
+
+#endif
---
+++ headers/chessBoard.hpp
@@ -0,0 +1,20 @@
+#ifndef CHESSBOARD_HPP
+#define CHESSBOARD_HPP
+
+#include "board.hpp"
+#include "tile.hpp"
+
+class Piece;
+
+class ChessBoard : public Board
+{
+    private:
+        Tile PlayBoard[8][8];
+
+    public:
+        virtual void initialize();
+        Tile getTile();
+        void setTile(Tile t, Piece p);
+};
+
+#endif
