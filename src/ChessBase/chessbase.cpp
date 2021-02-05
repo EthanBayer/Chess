389d8191429dfc6a7be51867c3f5368853e6a92d
EthanBayer <bayeres1871@gmail.com>
2021-02-05 14:58:05 -0800

    Added files for the ChessBase class, possible include error.

---
+++ src/ChessBase/chessbase.cpp
@@ -0,0 +1,58 @@
+
+#include "../../header/ChessBase/chessbase.hpp"
+
+ChessBase::ChessBase()
+{
+    representation = '0';
+    name = "No Name";
+    PieceStart = nullptr;
+}
+
+ChessBase::ChessBase(char r, std::string name)
+{
+    representation = r;
+    this->name = name;
+    PieceStart = nullptr;
+}
+
+ChessBase::ChessBase(char r, std::string name, ChessPieceStrategy* ps)
+{
+    representation = r;
+    this->name = name;
+    PieceStart = ps;
+}
+
+char ChessBase::getRep()
+{
+    return representation;
+}
+
+std::string ChessBase::getName()
+{
+    return name;
+}
+
+ChessPieceStrategy* ChessBase::getPieceStrat()
+{
+    return PieceStart;
+}
+
+std::array<std::array<Tile, 8>, 8> ChessBase::getBoard()
+{
+    return Board;
+}
+
+void ChessBase::setRep(char r)
+{
+    representation = r;
+}
+
+void ChessBase::setName(std::string n)
+{
+    this->name = name;
+}
+
+void ChessBase::setPieceStrat(ChessPieceStrategy* ps)
+{
+    PieceStart = ps;
+}
