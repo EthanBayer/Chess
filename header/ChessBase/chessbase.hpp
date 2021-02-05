b0aee63917aa4e4fa62227275755f51f9a96159f
EthanBayer <bayeres1871@gmail.com>
2021-02-05 15:00:22 -0800

    Added structures file and the chessbase.hpp file again.

---
+++ header/ChessBase/chessbase.hpp
@@ -0,0 +1,32 @@
+#ifndef CHESSBASE_HPP
+#define CHESSBASE_HPP
+
+#include <iostream>
+#include <array>
+#include "../structures.hpp"
+
+class ChessPieceStrategy;
+
+class ChessBase
+{
+    private:
+        char representation;
+        std::string name;
+        ChessPieceStrategy* PieceStart;
+        std::array<std::array<Tile, 8>, 8> Board;
+
+    public:
+        ChessBase();
+        ChessBase(char r, std::string name);
+        ChessBase(char r, std::string name, ChessPieceStrategy* ps);
+        char getRep();
+        std::string getName();
+        ChessPieceStrategy* getPieceStrat();
+        std::array<std::array<Tile, 8>, 8> getBoard();
+        void setRep(char r);
+        void setName(std::string n);
+        void setPieceStrat(ChessPieceStrategy* ps);
+        virtual void print() = 0;
+};
+
+#endif
---
+++ header/structures.hpp
@@ -0,0 +1,14 @@
+#ifndef STRUCTURES_HPP
+#define STRUCTURES_HPP
+
+#include "ChessBase/chessbase.hpp"
+
+//class ChessBase;
+
+struct Tile
+{
+    char color;
+    Chessbase* usedPiece;
+};
+
+#endif
